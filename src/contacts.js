2f1343da9e9d58d7a1b304ff65def9051c5747a7
Prakhar Srivastava <prakharshivam206@gmail.com>
2022-11-09 18:00:26 +0530

    Contacts

---
+++ src/contacts.js
@@ -0,0 +1,25 @@
+const contacts = [
+    {
+      name: "Sushant",
+      imgURL: "https://wallpaperaccess.com/full/2353453.jpg",
+      phone: "+985865478",
+      email: "Sustant@g.com"
+    },
+    {
+      name: "Virat Kohli",
+      imgURL:
+        "https://www.celebritycontactdetails.com/wp-content/uploads/2019/01/Cricketer-Virat-Kohli-Contact.jpg",
+      phone: "+987654321",
+      email: "Virat@g.com"
+    },
+    {
+      name: "MS Dhoni",
+      imgURL:
+        "https://cdn.wisden.com/wp-content/uploads/2017/11/GettyImages-689405274-e1520010556769.jpg",
+      phone: "+918372574",
+      email: "gmail@MSDhoni.com"
+    }
+  ];
+  
+  export default contacts;
+  
